f1a328fc62dd91a773451dae2692ced8165de332
gcerame <g.cerame@gmail.com>
2022-09-24 16:37:43 +0200

    Questions and answers rendering correctly

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,10 +1,29 @@
 import Question from "./Question";
+import React, {useEffect} from "react";
 
 export default function Game() {
+
+    const [questions, setQuestions] = React.useState([]);
+
+    useEffect(() => {
+        fetch('https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple')
+            .then(res => res.json())
+            .then(data => setQuestions(data.results));
+    }, []);
+
+    function generateQuestions() {
+        return questions.map(question => {
+            return <Question
+                question={question.question}
+                answers={[...question.incorrect_answers, question.correct_answer]}
+            />;
+        });
+    }
+
     return (
         <div className="game">
             <h1>Pantalla juego</h1>
-            <Question/>
+            {generateQuestions()}
         </div>
     );
 }
--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
+import he from "he";
+
 
 export default function Question(props) {
-    const question =
-        {
-            question: "¿Cuál es el nombre de la mascota de Facebook?",
-            answers: [{text: "Answer 1"},
-                {text: "Answer 2"},
-                {text: "Answer 3"},
-                {text: "Answer 4"}]
-        };
+
+    const [answers, setAnswers] = React.useState(props.answers);
+
 
     function generateAnswers() {
-        return question.answers.map(answer =>
+        console.log(props.answers);
+        return props.answers.map(answer =>
             <div className="answer">
-                <span>{answer.text}</span>
+                <span>{he.decode(answer)}</span>
             </div>);
     }
 
     return (
-        <>
-            <h2>{question.question}</h2>
-            <div className="answer">
+        <div className="question">
+            <h2>{he.decode(props.question)}</h2>
+            <div className="answers">
                 {generateAnswers()}
             </div>
-        </>
+        </div>
     );
 }
